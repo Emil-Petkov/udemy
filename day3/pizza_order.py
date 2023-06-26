4bd629cca0086e2adfe7c105865b2b01dd2f3600
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2023-06-26 16:33:15 +0300

    Create pizza_order.py

---
+++ day3/pizza_order.py
@@ -0,0 +1,34 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+total_bill = 0
+
+price_for_size = {
+    "S": 15,
+    "M": 20,
+    "L": 25,
+}
+
+peperoni = {
+    "S": 2,
+    "M": 3,
+    "L": 3,
+}
+
+cheese = {
+    "Y": 1,
+    "N": 0,
+}
+
+if size in price_for_size:
+    total_bill += price_for_size[size]
+
+    if add_pepperoni == "Y":
+        total_bill += peperoni[size]
+
+    if extra_cheese == "Y":
+        total_bill += cheese["Y"]
+
+print(f"Your final bill is: {total_bill}")
