dbdf20f1351b27bf6c45b85786e75f52f3e93261
emil.py <114022856+Emil-Petkov@users.noreply.github.com>
2024-02-06 21:14:40 +0200

    Create calculator.py

---
+++ day_10/calculator.py
@@ -0,0 +1,30 @@
+def calculator(f_num: float, operator: str, s_num: float):
+    if s_num == 0 and operator in ['/', '//', '%']:
+        return 'Divide by Zero.'
+
+    mapper_operation = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x / y,
+        '//': lambda x, y: x // y,
+        '%': lambda x, y: x % y,
+    }
+
+    if operator in mapper_operation:
+        return mapper_operation[operator](f_num, s_num)
+
+    else:
+        raise ValueError('Invalid operator.')
+
+
+first_number = float(input())
+operation = input()
+second_number = float(input())
+
+try:
+    result = calculator(first_number, operation, second_number)
+    print(f'Result is: {result}')
+
+except ValueError as e:
+    print(e)
